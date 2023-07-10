a5a378aaf35c8868be109d32f90d69a2baa5ccea
SamSKS <samuel.meireles.15s@outlook.com>
2023-07-09 22:51:07 -0300

    add: middleware ensureIsSuperuser

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,10 @@
 import ensureIsValidDataMiddleware from "./formHandling/ensureValidDataReq.middleware";
 import ensureEmailExistsMiddleware from "./validation/users/ensureEmailExists.middleware";
 import ensureUsernameExistsMiddleware from "./validation/users/ensureUsernameExists.middleware";
+import ensureIsSuperuserMiddleware from "./authorization/ensureIsSuperuser.middleware";
+
+// AUTHORIZATION
+export { ensureIsSuperuserMiddleware };
 
 // FORM HANDLING
 export { ensureIsValidDataMiddleware };
