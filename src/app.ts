f708a548e76261295a749d3a42e2c68e6db4f5a7
SamSKS <samuel.meireles.15s@outlook.com>
2023-09-07 16:16:39 -0300

    refactor: :recycle: update handleError for middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { Application, json } from "express";
 import "express-async-errors";
-import handleError from "./errors/handlerError";
 import "dotenv/config";
 import cors from "cors";
+import middlewares from "./middlewares";
 import usersRoutes from "./routes/users.routes";
 import authRoutes from "./routes/authSessions.routes";
 
@@ -15,6 +15,6 @@ const API_DETAIL: string = process.env.API_DETAIL || "";
 app.use(`${API_DETAIL}/auth`, authRoutes);
 app.use(`${API_DETAIL}/users`, usersRoutes);
 
-app.use(handleError);
+app.use(middlewares.handleError);
 
 export default app;
