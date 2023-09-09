e0a6b4cf5cc803aeb0ff6eb4ec2576b424e6f9ab
SamSKS <samuel.meireles.15s@outlook.com>
2023-09-08 21:30:25 -0300

    fix: :ambulance: repair and update activation user with avatar(pre - build)

--- src/services/users/enableUser.service.ts
+++ src/services/users/enableUser.service.ts
@@ -1,20 +1,41 @@
-import { iUser, iUserRes } from "../../interfaces/users.interface";
-import { userRepository } from "../../repositories";
+import path from "path";
+import {
+  iUser,
+  iUserRes,
+  iUserResLocals,
+  iUserResWithAvatar,
+} from "../../interfaces/users.interface";
+import { avatarRepository, userRepository } from "../../repositories";
 import userSchemas from "../../schemas/users.schema";
+import AppError from "../../errors/AppError";
 
 const enableUser = async (userId: string): Promise<iUserRes> => {
+  const user: iUser | null = await userRepository.findOne({
+    where: { id: userId },
+    relations: ["avatar"],
+    withDeleted: true,
+  });
+  // precisa verificar uuid valido e demais verificações de seguranã com middleware e código de ativação
+  if (!user) throw new AppError("User not found", 404);
+  if (user.deleted_at === null) throw new AppError("User already enabled", 401);
+
   await userRepository.restore(userId);
 
-  const user: iUser | null = await userRepository.findOneBy({ id: userId });
+  if (user.avatar) await avatarRepository.restore(user.avatar.id);
 
-  const { id, ...userData }: iUserRes = userSchemas.userUpdateRes.parse(user);
+  const userRestored: iUser | null = await userRepository.findOne({
+    where: { id: userId },
+    relations: ["avatar"],
+  });
 
-  const dataRes: iUserRes = {
+  const { id, ...userData }: iUserResLocals =
+    userSchemas.userResLocals.parse(userRestored);
+
+  return {
     id: id,
     ...userData,
-  };
-
-  return dataRes as iUserRes;
+    avatar: userData.avatar ? path.join(__dirname, userData.avatar.path) : null,
+  } as iUserResWithAvatar;
 };
 
 export default enableUser;
